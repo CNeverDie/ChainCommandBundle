0e2f02c547adec20453f2baf051ebb374591b3a2
Alexandr Borovets <cneverdie@gmail.com>
2016-08-25 12:26:58 +0300

    Added logging

--- Service/ChainManager.php
+++ Service/ChainManager.php
@@ -2,23 +2,28 @@
 
 namespace Borovets\ChainCommandBundle\Service;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ChainManager
 {
     /** @var ChainCollection */
     private $chainCollection;
+    private $logger;
 
     /**
      * ChainManager constructor.
      * @param ChainCollection $chainCollection
+     * @param LoggerInterface $logger
      */
-    public function __construct(ChainCollection $chainCollection)
+    public function __construct(ChainCollection $chainCollection, LoggerInterface $logger)
     {
         $this->chainCollection = $chainCollection;
+        $this->logger = $logger;
     }
 
     /**
@@ -38,9 +43,29 @@ class ChainManager
      */
     public function runChain(Command $command, $input, $output)
     {
+        $this->logger->info(
+            sprintf('%s is a master command of a command chain that has registered member commands',
+                $command->getName()
+            )
+        );
+
+        $chainMembers = $this->getChainedCommands($command->getName());
+
         $this->runMainCommand($command, $input, $output);
 
-        $this->runChainCommand($command->getName(), $output);
+        $this->logger->info(
+            sprintf('Executing %s command chain members:',
+                $command->getName()
+            )
+        );
+
+        $this->runChainCommands($chainMembers, $output);
+
+        $this->logger->info(
+            sprintf('Execution of %s chain completed.',
+                $command->getName()
+            )
+        );
     }
 
     /**
@@ -52,24 +77,41 @@ class ChainManager
      */
     private function runMainCommand(Command $command, $input, $output)
     {
-        $command->run($input, $output);
+        $this->logger->info(
+            sprintf('Executing %s command itself first:',
+                $command->getName()
+            )
+        );
+
+        $buffer = new BufferedOutput();
+        $command->run($input, $buffer);
+
+        $outputMessage = $buffer->fetch();
+
+        $this->logger->info($outputMessage);
+        $output->writeln($outputMessage);
     }
 
 
     /**
-     * @param string $chainName
-     * @param $output
+     * @param $chainCommands
+     * @param OutputInterface $output
      *
      * @TODO: Input params for chained command as service tags
      */
-    private function runChainCommand($chainName, $output)
+    private function runChainCommands($chainCommands, $output)
     {
-        $chain = $this->getChainedCommands($chainName);
-
-        foreach ($chain as $chainItem) {
+        foreach ($chainCommands as $chainItem) {
             /** @var Command $chainCommand */
             $chainCommand = $chainItem['command'];
-            $chainCommand->run(new ArrayInput([]), $output);
+
+            $buffer = new BufferedOutput();
+            $chainCommand->run(new ArrayInput([]), $buffer);
+
+            $outputMessage = $buffer->fetch();
+
+            $this->logger->info($outputMessage);
+            $output->writeln($outputMessage);
         }
     }
 
@@ -81,6 +123,17 @@ class ChainManager
      */
     private function getChainedCommands($mainCommand)
     {
-        return $this->chainCollection->getChainSubCommands($mainCommand);
+        $chainMembers = $this->chainCollection->getChainSubCommands($mainCommand);
+
+        foreach ($chainMembers as $chainMember) {
+            $this->logger->info(
+                sprintf('%s registered as a member of %s command chain',
+                    $chainMember['commandName'],
+                    $mainCommand
+                )
+            );
+        }
+
+        return $chainMembers;
     }
 }
